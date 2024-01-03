b4565d1a6ba06fd4c312b766ed6685e2de03172a
Xuanhao Ji <xuanhao.ji@greendotcorp.com>
2024-01-03 18:24:44 +0800

    clear code

--- WebClient/obj/Debug/net8.0/WebClient.AssemblyInfo.cs
+++ WebClient/obj/Debug/net8.0/WebClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ddb5e9352566911add122250f895c12eda443ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
